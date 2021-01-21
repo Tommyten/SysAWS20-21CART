3a86e88a51950b9b5a2f85badd14867deca481c4
thomas <thomas@horm.es>
2021-01-21 14:34:03 +0100

    Rebuilt so Categorization and Regression are both in class CART; CART class finds out (depending on whether the outputfield is float/double) whether problem is categorization or regression, then gets correct Strategy and uses it

---
+++ src/main/java/es/horm/cart/lib/tree/BinaryTreePrinter.java
@@ -0,0 +1,67 @@
+package es.horm.cart.lib.tree;
+
+import java.io.PrintStream;
+
+/**
+ * Source:
+ * https://www.baeldung.com/java-print-binary-tree-diagram
+ */
+public class BinaryTreePrinter {
+
+    private BinaryTree tree;
+
+    public BinaryTreePrinter(BinaryTree tree) {
+        this.tree = tree;
+    }
+
+    private String traversePreOrder(BinaryTree root) {
+
+        if (root == null) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(root.getRoot().getData());
+
+        String pointerRight = "└──";
+        String pointerLeft = (root.getRoot().getRight() != null) ? "├──" : "└──";
+
+        traverseNodes(sb, "", pointerLeft, root.getRoot().getLeft(), root.getRoot().getRight() != null);
+        traverseNodes(sb, "", pointerRight, root.getRoot().getRight(), false);
+
+        return sb.toString();
+    }
+
+    private void traverseNodes(StringBuilder sb, String padding, String pointer, Node node,
+                               boolean hasRightSibling) {
+
+        if (node != null) {
+
+            sb.append("\n");
+            sb.append(padding);
+            sb.append(pointer);
+            sb.append(node.getData());
+
+            StringBuilder paddingBuilder = new StringBuilder(padding);
+            if (hasRightSibling) {
+                paddingBuilder.append("│  ");
+            } else {
+                paddingBuilder.append("   ");
+            }
+
+            String paddingForBoth = paddingBuilder.toString();
+            String pointerRight = "└──";
+            String pointerLeft = (node.getRight() != null) ? "├──" : "└──";
+
+            traverseNodes(sb, paddingForBoth, pointerLeft, node.getLeft(), node.getRight() != null);
+            traverseNodes(sb, paddingForBoth, pointerRight, node.getRight(), false);
+
+        }
+
+    }
+
+    public void print(PrintStream os) {
+        os.print(traversePreOrder(tree));
+    }
+
+}
